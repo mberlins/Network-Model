a6a5a7af479777e6cf7d31a0569bfaba45a7c5c4
mberlins <62808050+mberlins@users.noreply.github.com>
2021-01-04 15:07:29 +0100

    Dodane zapisywanie wszystkich zbiorów
    Dodane wszystkie parametry z wyjątkiem stream_cost

--- Reader.py
+++ Reader.py
@@ -115,18 +115,109 @@ print(len(selectedCities), len(selectedLinks), len(selectedDemands))
 file = open("C:\\Users\\Admin\\PycharmProjects\\NetworkModel\\PP.dat", "w")
 
 file.write("set VERT :=")
-
 for city in selectedCities:
     file.write(" ")
     file.write(str(city.id))
 
 file.write("; \n\n")
 file.write("set ARCS :=")
-
 for link in selectedLinks:
     file.write(" ")
     file.write(str(link.id))
 
 file.write("; \n\n")
+file.write("set STREAMS :=")
+for stream in selectedDemands:
+    file.write(" ")
+    file.write(str(stream.id))
+
+file.write("; \n\n")
+file.write("set VEHICLES := 1 2 3 4")
+
+file.write("; \n\n")
+file.write("set DIM := 1 2")
+
+file.write("; \n\n")
+file.write("set LOG_FUN := NONE WER PER")
+
+file.write("; \n\n")
+file.write("set DELAYS := DELAYSB DELAYS DELAY DELA DEL")
+
+file.write("; \n\n")
+for vertex in selectedCities:
+    file.write("set ARCSE[")
+    file.write(str(vertex.id))
+    file.write("] :=")
+    for arc in links:
+        if arc.source == vertex.name:
+            file.write(" ")
+            file.write(str(arc.id))
+    file.write(";\n")
+
+file.write("\n\n")
+for vertex in selectedCities:
+    file.write("set ARCSL[")
+    file.write(str(vertex.id))
+    file.write("] :=")
+    for arc in links:
+        if arc.target == vertex.name:
+            file.write(" ")
+            file.write(str(arc.id))
+    file.write(";\n")
+
+file.write("\n\n")
+file.write("param source :=\n")
+for stream in selectedDemands:
+    file.write("\t")
+    file.write(str(stream.id))
+    file.write("\t")
+    for city in selectedCities:
+        if stream.source == city.name:
+            file.write(str(city.id))
+            break
+    file.write("\n")
+
+file.write(";\n\n")
+file.write("param dest :=\n")
+for stream in selectedDemands:
+    file.write("\t")
+    file.write(str(stream.id))
+    file.write("\t")
+    for city in selectedCities:
+        if stream.target == city.name:
+            file.write(str(city.id))
+            break
+    file.write("\n")
+
+file.write(";\n\n")
+file.write("param stream_size (tr):")
+for stream in selectedDemands:
+    file.write("\t")
+    file.write(str(stream.id))
+file.write(":=\n")
+
+i = 1
+while i < 3:
+    file.write("\t\t")
+    file.write(str(i))
+    for stream in selectedDemands:
+        file.write("\t")
+        value = float(stream.demand_value)
+        value = int(value)
+        file.write(str(value))
+    file.write("\n")
+    i = i + 1
+
+file.write(";\n\n")
+file.write("param max_delay:=\n")
+for stream in selectedDemands:
+    file.write("\t")
+    file.write(str(stream.id))
+    file.write("\t")
+    file.write("DELAYSB")
+    file.write("\n")
+
+file.write(";\n\n")
+file.write("param travel_cost (tr):")                       #TODO obliczanie dlugości łuków, geograficzne ograniczenia dla miast
 
 file.close()
